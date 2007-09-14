77155070c4724eba139999ad479ce2a0c0947a64
snoopyhzy@gmail.com <snoopyhzy@gmail.com>
2007-09-14 11:15:05 +0000

    中文输入,默认语言改为英语

--- SieJC/lang.h
+++ SieJC/lang.h
@@ -6,10 +6,10 @@
 #define _LANG_H_
 
 #define LG_COPYRIGHT "Siemens natJabber Client\n(c)Kibab, Ad, Borman99\n%s r%d\nCompiled %s"
-#define LANG_EN
+#define LANG_ORIGINAL
 //#define LANG_RU
 //#define LANG_UA
-//#define LANG_EN
+#define LANG_EN
 ///////////////////////////////////////////////////////////////////////////////////////////
 #ifdef LANG_ORIGINAL
 #define LG_SELECT "Выбор"
