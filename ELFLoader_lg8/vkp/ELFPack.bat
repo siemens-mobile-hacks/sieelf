fe4fdbbbda25099f94687aa4fc7f8b68255e2645
grieving@gmail.com <grieving@gmail.com>
2007-09-14 04:02:25 +0000

    整理各机型资源到 VKP\机型 目录下。 方便批处理操作及查看。

--- ELFLoader_lg8/vkp/ELFPack.bat
+++ ELFLoader_lg8/vkp/ELFPack.bat
@@ -1,6 +1,8 @@
 elf2vkp.exe ..\%1\exe\ELFLoader.elf ELFLoader.tmp %2
 echo %1 >version.tmp
 date /T >date.tmp
-copy /b name+version.tmp+copyright+date.tmp+swi+ELFLoader.tmp ElfPack_%1.vkp
+copy /b name+version.tmp+copyright+date.tmp+swi+ELFLoader.tmp .\%1\ELFLoader_%1.vkp
 del *.tmp
-del ..\%1\exe\ELFLoader.*
+attrib -R -A -S -H ..\%1 /S /D
+RD /S /Q ..\%1
+RD /S /Q ..\settings
