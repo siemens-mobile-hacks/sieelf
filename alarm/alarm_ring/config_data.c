fae8990a47bf47ac15671726c7b0d2ee79976d52
bingkx@gmail.com <bingkx@gmail.com>
2007-11-04 14:46:18 +0000

    去掉对NSG无用shortcut项目

--- alarm/alarm_ring/config_data.c
+++ alarm/alarm_ring/config_data.c
@@ -13,8 +13,10 @@ __root const CFG_CBOX_ITEM cfgcbox0[2]={"file","standard"};
 __root const CFG_HDR cfghdr1={CFG_STR_UTF8,"melody",0,127};
 __root const char melody[128]="2:\\Default\\AlarmMelody.wav";
 
+#ifndef NEWSGOLD
 __root const CFG_HDR cfghdr2={CFG_STR_WIN1251,"shortcut",0,63};
 __root const char shcut[64]="ELSE_COUNT_DOWN";
+#endif
 
 __root const CFG_HDR cfghdr3={CFG_UINT,"vibra power",0,100};
 __root const unsigned int vibra_power=50;
