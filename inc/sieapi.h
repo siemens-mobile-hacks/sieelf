9713f1f145dae630158e092bdad90ffa8035110a
comerose@126.com <comerose@126.com>
2007-11-08 17:08:55 +0000

    更新一些函数!

--- inc/sieapi.h
+++ inc/sieapi.h
@@ -70,6 +70,9 @@ _C_LIB_DECL
  __INTRINSIC void RunCUT(char *s);
  __INTRINSIC void RunAPP(char *s);
  __INTRINSIC void RunADR(char *s);
+ __INTRINSIC TDate GetOldDay(TDate pSt);
+ __INTRINSIC char *LoadFileBuf(const char *FileName);
+ __INTRINSIC int  FreeFileBuf(char *Buffer);
 _END_C_LIB_DECL
 _C_STD_END
 //函数执行代码
@@ -142,7 +145,80 @@ void BSTRAdd(word *pDst, const word * pSrc, int Count)
   while(*pSrc != '\0' && i<=Count) *(pDst+ nSize + i++) = *pSrc++;
   *pDst = nSize+i-1;
 }
-#pragma inline//生成农历数据
+
+#pragma inline//当前日期转换为农历日期
+TDate GetOldDay(TDate pSt)
+{/*公历每月前面的天数*/
+const int wMonthAdd[12] = {0,31,59,90,120,151,181,212,243,273,304,334};
+/*农历数据*/
+const int wNongliData[100] = {2635  ,333387,1701,1748,267701,694,2391,133423,1175,396438
+                             ,3402  ,3749  ,331177,1453,694,201326,2350,465197,3221,3402
+                             ,400202,2901  ,1386,267611,605,2349,137515,2709,464533,1738
+                             ,2901  ,330421,1242,2651,199255,1323,529706,3733,1706,398762
+                             ,2741  ,1206  ,267438,2647,1318,204070,3477,461653,1386,2413
+                             ,330077,1197  ,2637,268877,3365,531109,2900,2922,398042,2395
+                             ,1179  ,267415,2635,661067,1701,1748,398772,2742,2391,330031
+                             ,1175  ,1611  ,200010,3749,527717,1452,2742,332397,2350,3222
+                             ,268949,3402  ,3493,133973,1386,464219,605,2349,334123,2709
+                             ,2890  ,267946,2773,592565,1210,2651,395863,1323,2707,265877};
+static int wCurYear,wCurMonth,wCurDay;
+static int nTheDate,nIsEnd,m,k,n,i,nBit;
+TDate OldDay;
+/*---取当前公历年、月、日---*/
+wCurYear  = pSt.year;
+wCurMonth = pSt.month;
+wCurDay   = pSt.day;
+/*---计算到初始时间1921年2月8日的天数：1921-2-8(正月初一)---*/
+nTheDate = (wCurYear - 1921) * 365 + (wCurYear - 1921) / 4 + wCurDay + wMonthAdd[wCurMonth - 1] - 38;
+if((!(wCurYear % 4)) && (wCurMonth > 2))
+  nTheDate = nTheDate + 1;
+
+/*--计算农历天干、地支、月、日---*/
+nIsEnd = 0;
+m      = 0;
+while(nIsEnd != 1)
+{
+  if(wNongliData[m] < 4095)  k = 11; else k = 12;
+  n = k;
+  while(n>=0)
+  {
+   //获取wNongliData(m)的第n个二进制位的值
+   nBit = wNongliData[m];
+   for(i=1;i<n+1;i++)
+       nBit = nBit/2;
+
+   nBit = nBit % 2;
+
+   if (nTheDate <= (29 + nBit))
+   {
+    nIsEnd = 1;
+    break;
+   }
+
+   nTheDate = nTheDate - 29 - nBit;
+   n = n - 1;
+  }
+  if(nIsEnd)
+   break;
+  m = m + 1;
+}
+wCurYear  = 1921 + m;
+wCurMonth = k - n + 1;
+wCurDay   = nTheDate;
+if (k == 12)
+{
+  if (wCurMonth == wNongliData[m] / 65536 + 1)
+      wCurMonth = 1 - wCurMonth;
+  else if (wCurMonth > wNongliData[m] / 65536 + 1)
+      wCurMonth = wCurMonth - 1;
+}
+OldDay.year =wCurYear;
+OldDay.month=wCurMonth;
+OldDay.day  =wCurDay;
+return(OldDay);
+}
+
+#pragma inline//获取农历日期信息
 void GetDayOf(TDate pSt,TNongLi *NongLiData)
 { 
 /*天干名称*/
@@ -185,86 +261,33 @@ const int wNongliData[100] = {2635  ,333387,1701,1748,267701,694,2391,133423,117
                              ,1175  ,1611  ,200010,3749,527717,1452,2742,332397,2350,3222
                              ,268949,3402  ,3493,133973,1386,464219,605,2349,334123,2709
                              ,2890  ,267946,2773,592565,1210,2651,395863,1323,2707,265877};
-static int wCurYear,wCurMonth,wCurDay;
-static int nTheDate,nIsEnd,m,k,n,i,nBit;
-word UniToday[5];
-/*---取当前公历年、月、日---*/
-wCurYear = pSt.year;
-wCurMonth = pSt.month;
-wCurDay = pSt.day;
-/*---计算到初始时间1921年2月8日的天数：1921-2-8(正月初一)---*/
-nTheDate = (wCurYear - 1921) * 365 + (wCurYear - 1921) / 4 + wCurDay + wMonthAdd[wCurMonth - 1] - 38;
-if((!(wCurYear % 4)) && (wCurMonth > 2))
-  nTheDate = nTheDate + 1;
-
-/*--计算农历天干、地支、月、日---*/
-nIsEnd = 0;
-m = 0;
-while(nIsEnd != 1)
-{
-  if(wNongliData[m] < 4095)
-   k = 11;
-  else
-   k = 12;
-  n = k;
-  while(n>=0)
-  {
-   //获取wNongliData(m)的第n个二进制位的值
-   nBit = wNongliData[m];
-   for(i=1;i<n+1;i++)
-    nBit = nBit/2;
-
-   nBit = nBit % 2;
-
-   if (nTheDate <= (29 + nBit))
-   {
-    nIsEnd = 1;
-    break;
-   }
-
-   nTheDate = nTheDate - 29 - nBit;
-   n = n - 1;
-  }
-  if(nIsEnd)
-   break;
-  m = m + 1;
-}
-wCurYear = 1921 + m;
-wCurMonth = k - n + 1;
-wCurDay = nTheDate;
-if (k == 12)
-{
-  if (wCurMonth == wNongliData[m] / 65536 + 1)
-   wCurMonth = 1 - wCurMonth;
-  else if (wCurMonth > wNongliData[m] / 65536 + 1)
-   wCurMonth = wCurMonth - 1;
-}
-
+TDate old = GetOldDay(pSt);
 /*--生成农历天干、地支、属相 ==> wNongli--*/
+word UniToday[5];
 CutWSTR(NongLiData->year,0);
-UniToday[0] = cTianGan[((wCurYear - 4) % 60) % 10];   //天干
-UniToday[1] = cDiZhi[((wCurYear - 4) % 60) % 12];     //地支
-UniToday[2] = cShuXiang[((wCurYear - 4) % 60) % 12];  //属相
+UniToday[0] = cTianGan[ ((old.year - 4) % 60) % 10];  //天干
+UniToday[1] = cDiZhi[   ((old.year - 4) % 60) % 12];  //地支
+UniToday[2] = cShuXiang[((old.year - 4) % 60) % 12];  //属相
 UniToday[3] = cOtherName[0];                          //年
 BSTRAdd(NongLiData->year->wsbody,UniToday,4);
 /*--生成农历月 --*/
 CutWSTR(NongLiData->mday,0);
-if (wCurMonth < 1)                            //闰月
+if (old.month < 1)                            //闰月
 {
     UniToday[0] = cOtherName[2];                //闰
-    UniToday[1] = cMonName[-1 * wCurMonth];
+    UniToday[1] = cMonName[-1 * old.month];
     BSTRAdd(NongLiData->mday->wsbody,UniToday,2);
 }
 else
 {
-    UniToday[0] = cMonName[wCurMonth];
+    UniToday[0] = cMonName[old.month];
     BSTRAdd(NongLiData->mday->wsbody,UniToday,1);
 }
   UniToday[0] = cOtherName[1];                //月
   BSTRAdd(NongLiData->mday->wsbody,UniToday,1);
 /*--生成农历日 --*/  
-  UniToday[0] = cDayName[wCurDay][0];  
-  UniToday[1] = cDayName[wCurDay][1]; 
+  UniToday[0] = cDayName[old.day][0];  
+  UniToday[1] = cDayName[old.day][1]; 
   BSTRAdd(NongLiData->mday->wsbody,UniToday,2);  
 }  
 
@@ -471,7 +494,35 @@ void DrawStringExt(WSHDR *ws,RECT rc,TFont Font,int text_attribute)
 #pragma inline
 void DrawStringRect(WSHDR *ws,TRect rc,TFont Font,int text_attribute)
 { DrawString(ws, rc.l, rc.t, rc.r, rc.b, Font.Size,text_attribute,Font.Pen, Font.Brush); }
-
+#pragma inline//创建文件缓冲区
+char *LoadFileBuf(const char *FileName)
+{
+  int f;
+  char *buf=0;  
+  uint err,fsize;  
+  if ((f=fopen(FileName,A_ReadOnly+A_BIN,P_READ,&err))!=-1)
+  {
+   fsize  =lseek(f,0,S_END,&err,&err);
+   if (fsize<=0){
+     fclose(f,&err);     
+   }else{       
+    lseek(f,0,S_SET,&err,&err);
+    buf=malloc(fsize+1);
+    fread(f,buf,fsize,&err);    
+    fclose(f,&err); 
+    buf[fsize]=0;   
+   }
+  }
+  return(buf);
+}
+#pragma inline//释放文件缓冲区
+int FreeFileBuf(char *Buffer)
+{
+  if(Buffer){
+    mfree(Buffer);
+    return(1);
+  }else return(0);
+}
 
 #endif/*SIEAPI_H_*/
 
@@ -504,6 +555,9 @@ void DrawStringRect(WSHDR *ws,TRect rc,TFont Font,int text_attribute)
  using _CSTD DrawStringRect;
  using _CSTD CutFileExt;
  using _CSTD getstrpos;
+ using _CSTD GetOldDay;
+ using _CSTD LoadFileBuf;
+ using _CSTD FreeFileBuf;
 #endif /* 导出函数引用表 */
  
 
