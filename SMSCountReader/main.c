5cc92de8b38a35e359e9b2808e263752c2b7c7f1
bingkx@gmail.com <bingkx@gmail.com>
2007-11-09 14:56:50 +0000

    一个简单的读取短信计数器SMSCOUNT.DAT的ELF

---
+++ SMSCountReader/main.c
@@ -0,0 +1,229 @@
+#include "..\inc\swilib.h"
+
+/*****************************************************************************
+00000000h: 0B 00 00 00 4E 00 00 00 42 00 00 00 00 00 00 00 ; ....N...B.......
+00000010h: 0C 00 00 00                                     ; ....
+*****************************************************************************/
+
+typedef struct
+{
+  CSM_RAM csm;
+  int gui_id;
+}MAIN_CSM;
+
+typedef struct
+{
+  GUI gui;
+}MAIN_GUI;
+
+extern void kill_data(void *p, void (*func_p)(void *));
+
+unsigned int MAINCSM_ID = 0;
+unsigned int MAINGUI_ID = 0;
+int screenw;
+int screenh;
+char* data_buf;
+
+#pragma inline
+int get_string_width(WSHDR *ws, int font)
+{
+  int width=0;
+  unsigned short *body=ws->wsbody;
+  int len=body[0];
+  while(len)
+  {
+    width+=GetSymbolWidth(body[len],font);
+    len--;
+  }
+  return (width);
+}
+
+#pragma inline
+void patch_rect(RECT*rc,int x,int y, int x2, int y2)
+{
+	rc->x=x;
+	rc->y=y;
+	rc->x2=x2;
+	rc->y2=y2;
+}
+
+void exit(void)
+{
+	CloseCSM(MAINCSM_ID);
+}
+
+void load_dat(void)
+{
+	unsigned int err;
+	int f;
+	if((f=fopen("2:\\SMSCount.dat",A_ReadOnly+A_BIN,P_READ,&err))==-1)
+  {
+  	if((f=fopen("2:\\SMSCount\\SMSCount.dat",A_ReadOnly+A_BIN,P_READ,&err))==-1)
+    {
+    	ShowMSG(1, (int)"Read data error");
+    	exit();
+    }
+  }
+  fread(f,data_buf,20,&err);
+  fclose(f,&err);
+}
+
+void soft_key(void)
+{
+  WSHDR *wsr = AllocWS(16);
+  wsprintf(wsr, "Exit");
+  DrawString(wsr,screenw-get_string_width(wsr,FONT_MEDIUM)-4,screenh-GetFontYSIZE(FONT_MEDIUM)-2,screenw,screenh,FONT_MEDIUM,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+  FreeWS(wsr);
+}
+
+void onRedraw(MAIN_GUI *data)
+{
+	DrawRectangle(0,YDISP,screenw,screenh,0,GetPaletteAdrByColorIndex(1),GetPaletteAdrByColorIndex(1));
+	soft_key();
+	WSHDR *ws = AllocWS(128);
+	wsprintf(ws, "All: %d\nChinaMobile: %d\nChinaUnicom: %d\nOther: %d", data_buf[4], data_buf[8], data_buf[0xC], data_buf[0x10]);
+	DrawString(ws,0,YDISP+screenh/10,screenw,screenh,FONT_MEDIUM,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+	FreeWS(ws);
+}
+
+int OnKey(MAIN_GUI *data, GUI_MSG *msg)
+{
+	if(msg->gbsmsg->msg==KEY_UP)
+	{
+		if(msg->gbsmsg->submess==RIGHT_SOFT)
+			exit();
+	}
+	return(0);
+}
+
+void onCreate(MAIN_GUI *data, void *(*malloc_adr)(int))
+{
+  data->gui.state=1;
+}
+
+void onClose(MAIN_GUI *data, void (*mfree_adr)(void *))
+{
+  data->gui.state=0;
+}
+
+void onFocus(MAIN_GUI *data, void *(*malloc_adr)(int), void (*mfree_adr)(void *))
+{
+  data->gui.state=2;
+  DisableIDLETMR();
+}
+
+void onUnfocus(MAIN_GUI *data, void (*mfree_adr)(void *))
+{
+  if (data->gui.state!=2) return;
+  data->gui.state=1;
+}
+
+int method8(void){return(0);}
+int method9(void){return(0);}
+
+const void * const gui_methods[11]={
+	(void *)onRedraw,	//Redraw
+	(void *)onCreate,	//Create
+	(void *)onClose,	//Close
+	(void *)onFocus,	//Focus
+	(void *)onUnfocus,	//Unfocus
+	(void *)OnKey,		//OnKey
+	0,
+	(void *)kill_data,	//Destroy
+	(void *)method8,
+	(void *)method9,
+	0
+};
+
+const RECT Canvas={0,0,0,0};
+
+void maincsm_oncreate(CSM_RAM *data)
+{
+  screenw=ScreenW();
+  screenh=ScreenH();
+  data_buf=malloc(32);
+  load_dat();
+	MAIN_GUI *main_gui=malloc(sizeof(MAIN_GUI));
+  MAIN_CSM *csm=(MAIN_CSM *)data;
+	zeromem(main_gui,sizeof(MAIN_GUI));	
+	patch_rect((RECT*)&Canvas,0,0,screenw-1,screenh-1);
+	main_gui->gui.canvas=(void *)(&Canvas);
+	main_gui->gui.flag30=2;
+	main_gui->gui.methods=(void *)gui_methods;
+	main_gui->gui.item_ll.data_mfree=(void (*)(void *))mfree_adr();	
+	
+  csm->csm.state=0;
+  csm->csm.unk1=0;
+  csm->gui_id=CreateGUI(main_gui);
+  
+  MAINGUI_ID=csm->gui_id;
+}
+
+int maincsm_onmessage(CSM_RAM *data, GBS_MSG *msg)
+{
+  MAIN_CSM *csm=(MAIN_CSM*)data;
+  if ((msg->msg==MSG_GUI_DESTROYED)&&((int)msg->data0==csm->gui_id))
+  {
+    csm->csm.state=-3;
+  }
+	return(1);
+}
+
+void Killer(void)
+{
+  extern void *ELF_BEGIN;
+  mfree(data_buf);
+  kill_data(&ELF_BEGIN,(void (*)(void *))mfree_adr());
+}
+
+void maincsm_onclose(CSM_RAM *csm)
+{
+  SUBPROC((void *)Killer);
+}
+
+const int minus11=-11;
+unsigned short maincsm_name_body[140];
+
+const struct
+{
+  CSM_DESC maincsm;
+  WSHDR maincsm_name;
+}MAINCSM =
+{
+  {
+    maincsm_onmessage,
+    maincsm_oncreate,
+#ifdef NEWSGOLD
+0,
+0,
+0,
+0,
+#endif
+maincsm_onclose,
+sizeof(MAIN_CSM),
+1,
+&minus11
+  },
+  {
+    maincsm_name_body,
+    NAMECSM_MAGIC1,
+    NAMECSM_MAGIC2,
+    0x0,
+    139
+  }
+};
+
+void UpdateCSMname(void) 
+{
+  wsprintf((WSHDR *)(&MAINCSM.maincsm_name), "SMSCountReader");
+}
+
+int main(void)
+{
+  char dummy[sizeof(MAIN_CSM)];
+  UpdateCSMname();
+  LockSched();
+  MAINCSM_ID=CreateCSM(&MAINCSM.maincsm,dummy,0);
+  UnlockSched();
+  return 0;
+}
