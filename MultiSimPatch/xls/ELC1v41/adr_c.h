65acef75af7fa1293e25342633243d236b016627
grieving@gmail.com <grieving@gmail.com>
2007-12-22 11:28:57 +0000

    *ELC1v41/adr_c.h 修正一个移植错误。

--- MultiSimPatch/xls/ELC1v41/adr_c.h
+++ MultiSimPatch/xls/ELC1v41/adr_c.h
@@ -1,7 +1,7 @@
 #define SIM_Access_Function_ADR   0xA0592988//A0578AAC
 #define RegisterInNetwork_ADR     0xA09E8CEF  
 #define GetAllCtxInfo_ADR         0xA060EBEF  
-#define Set_Profile_ADR           0xA060F11A
+#define Set_Profile_ADR           0xA060F11B
 #define Set_LAI_ADR               0xA0D18D9F//A0CE5D2B
 #define SetEEFULLBlock_ADR        0xA0502F68//A04EC808
 #define GetEEFULLBlock_ADR        0xA0502CD8//A04EC578
