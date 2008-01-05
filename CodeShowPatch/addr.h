acec590e52919f19cb7b324f570f562921f79dad
bingkx <bingkx>
2008-01-05 06:38:46 +0000

    +S7Cv47,ELC1v41

--- CodeShowPatch/addr.h
+++ CodeShowPatch/addr.h
@@ -16,3 +16,39 @@
 
 #endif
 
+#ifdef S7Cv47
+#define ADDR_strlen 0xA14270BC+1
+#define ADDR_atou 0xA02C6AA0+1
+#define ADDR_WS_InitByZero	0xA01F7A76+1
+#define ADDR_GetCalleeNumber	0xA09833EE+1
+#define ADDR_UpdateWndItem	0xA0A35900+1
+#define ADDR_memcmp	0xA142627C+1
+
+#define ADDR_RECORD1 0xA06A0FE8
+#define ADDR_NUMX 0xA06A0FF4
+#define ADDR_IsCalling 0xA0DE7EA4+1
+#define ADDR_CallIN 0xA02FCB7C+1
+#define ADDR_CallOUT 0xA02FCD22+1
+#define ADDR_ADDRBook 0xA01F8062+1
+
+#endif
+
+#ifdef ELC1v41
+#define ADDR_strlen 0xA0FFCF8C+1
+#define ADDR_atou 0xA06471E4+1
+#define ADDR_WS_InitByZero	0xA05162CA+1
+#define ADDR_GetCalleeNumber	0xA095391E+1
+#define ADDR_UpdateWndItem	0xA0A55234+1
+#define ADDR_memcmp	0xA0FFC134+1
+
+#define ADDR_RECORD1 0xA0C4D128
+#define ADDR_NUMX 0xA0C4D09C
+#define ADDR_IsCalling 0xA0D158C8+1
+#define ADDR_CallIN 0xA0681B64+1
+#define ADDR_CallOUT 0xA0681E44+1
+#define ADDR_ADDRBook 0xA05168B6+1
+
+#endif
+
+
+
