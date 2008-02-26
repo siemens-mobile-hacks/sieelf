8162769ed79f641cbe2673b020e2ae908742ebf0
bingkx <bingkx>
2008-02-26 03:08:56 +0000

    冒号问题解决

--- TED/config_data.c
+++ TED/config_data.c
@@ -6,15 +6,15 @@
 #define DEFAULT_DISK "0"
 #endif
 
-//Конфигурация
+//Конфигурац?
 __root const CFG_HDR cfghdr0={CFG_UINT,"Ink color",0,22};
 __root const unsigned int INK=0;
 
-__root const CFG_HDR cfghdr1={CFG_UINT,"Paper colorі",0,22};
+__root const CFG_HDR cfghdr1={CFG_UINT,"Paper color",0,22};
 __root const unsigned int PAPER=1;
 
 __root const CFG_HDR cfghdr2={CFG_STR_UTF8,"Files path",3,63};
-__root const char ted_path[64]=DEFAULT_DISK ":\\ZBin\\TED\\";
+__root const char ted_path[64]=DEFAULT_DISK":\\ZBin\\TED\\";
 
 __root const CFG_HDR cfghdr3={CFG_UINT,"Display light",0,100};
 __root const unsigned int DISPLAY_LIGHT=10;
