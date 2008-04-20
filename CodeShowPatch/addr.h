5f8311b441c8783895300517ac840617f374310e
bingkx <bingkx>
2008-04-20 10:46:13 +0000

    SK6Cv50, V1.3-> 增加短信发送动画界面对所发送短信号码的区号秀支持 main.asm整理一下代码，SGOLD的HOOK部分和NEWSGOLD/ELKA分开来，独立出了添加换行的函数

--- CodeShowPatch/addr.h
+++ CodeShowPatch/addr.h
@@ -8,7 +8,7 @@
 #define ADDR_memcmp	0xA0FF0DA4+1
 
 #define ADDR_RECORD1 0xA0C45170
-#define ADDR_NUMX 0xA0C450CC
+#define ADDR_wsAppendChar 0xA0C450CC
 #define ADDR_IsCalling 0xA0D0CF80+1
 #define ADDR_CallIN 0xA067DACC+1
 #define ADDR_CallOUT 0xA067DDAC+1
@@ -18,21 +18,21 @@
 #endif
 
 #ifdef S7Cv47
-#define ADDR_strlen 0xA14270BC+1
-#define ADDR_atou 0xA02C6AA0+1
+#define ADDR_strlen		0xA14270BC+1
+#define ADDR_atou		0xA02C6AA0+1
 #define ADDR_WS_InitByZero	0xA01F7A76+1
 #define ADDR_GetCalleeNumber	0xA09833EE+1
 #define ADDR_UpdateWndItem	0xA0A35900+1
-#define ADDR_memcmp	0xA142627C+1
+#define ADDR_memcmp		0xA142627C+1
 
-#define ADDR_RECORD1 0xA06A0FE8
-#define ADDR_NUMX 0xA06A0FF4
-#define ADDR_IsCalling 0xA0DE7EA4+1
-#define ADDR_CallIN 0xA02FCB7C+1
-#define ADDR_CallOUT 0xA02FCD22+1
-#define ADDR_ADDRBook 0xA01F8062+1
+#define ADDR_RECORD1		0xA06A0FE8
+#define ADDR_wsAppendChar	0xA06A0FF4
+#define ADDR_IsCalling		0xA0DE7EA4+1
+#define ADDR_CallIN		0xA02FCB7C+1
+#define ADDR_CallOUT		0xA02FCD22+1
+#define ADDR_ADDRBook		0xA01F8062+1
 
-#define BASEADDRESS			0xA1580000
+#define BASEADDRESS		0xA1580000
 #endif
 
 #ifdef ELC1v41
@@ -44,7 +44,7 @@
 #define ADDR_memcmp	0xA0FFC134+1
 
 #define ADDR_RECORD1 0xA0C4D128
-#define ADDR_NUMX 0xA0C4D09C
+#define ADDR_wsAppendChar 0xA0C4D09C
 #define ADDR_IsCalling 0xA0D158C8+1
 #define ADDR_CallIN 0xA0681B64+1
 #define ADDR_CallOUT 0xA0681E44+1
@@ -54,20 +54,21 @@
 #endif
 
 #ifdef SK6Cv50
-#define ADDR_strlen 0xA0D9626C+1
-#define ADDR_atou 0xA020E4CC+1
+#define ADDR_strlen		0xA0D9626C+1
+#define ADDR_atou		0xA020E4CC+1
 #define ADDR_WS_InitByZero	0xA07197EA+1
 #define ADDR_GetCalleeNumber	0xA0733432+1
 #define ADDR_UpdateWndItem	0xA0999A5E+1
-#define ADDR_memcmp	0xA0D958AC+1
+#define ADDR_memcmp		0xA0D958AC+1
 
-#define ADDR_RECORD1 0xA05928C4
-#define ADDR_NUMX 0xA05927D4
-#define ADDR_CALLX 0xA044AC5C
-#define ADDR_CallIN 0xA038F496+1
-#define ADDR_CallOUT 0xA038E854+1
-#define ADDR_ADDRBook 0xA0592670
-#define BASEADDRESS			0xA0E30000
+#define ADDR_RECORD1		0xA05928C4
+#define ADDR_wsAppendChar	0xA0719AE0+1
+#define ADDR_CALLX		0xA044AC5C
+#define ADDR_CallIN		0xA038F496+1
+#define ADDR_CallOUT		0xA038E854+1
+#define ADDR_ADDRBook		0xA0592670
+#define ADDR_SMS_SEND		0xA07D3BC0+1	
+#define BASEADDRESS		0xA0E30000
 
 
 #endif
@@ -81,7 +82,7 @@
 #define ADDR_memcmp	0xA11054D4+1
 
 #define ADDR_RECORD1 0xA09F2E50
-#define ADDR_NUMX 0xA09F2D8C
+#define ADDR_wsAppendChar 0xA09F2D8C
 #define ADDR_IsCalling 0xA0C26E00+1
 #define ADDR_CallIN 0xA02E8BA8+1
 #define ADDR_CallOUT 0xA02E8D4E+1
@@ -99,7 +100,7 @@
 #define ADDR_memcmp	0xA14173C0+1
 
 #define ADDR_RECORD1 0xA0BB5114
-#define ADDR_NUMX 0xA0BB5050
+#define ADDR_wsAppendChar 0xA0BB5050
 #define ADDR_IsCalling 0xA0DE44C8+1
 #define ADDR_CallIN 0xA02FB630+1
 #define ADDR_CallOUT 0xA02FB7D6+1
--- CodeShowPatch/main.asm
+++ CodeShowPatch/main.asm
@@ -6,9 +6,6 @@
     EXTERN  do_phonebook_work 
 
     RSEG  CODE
-    
-//SSS:
-//		DCB	 "%s",00
 		
     CODE16
     
@@ -53,7 +50,13 @@ UpdateWndItem:
     LDR   R4, =ADDR_UpdateWndItem
     BLX   R4
     POP   {R4, PC}
-
+    
+AddNewLine:
+	PUSH		{R0-R7, LR}
+	MOV		R1, #0xD
+	LDR		R2, =ADDR_wsAppendChar
+	BLX		R2
+	POP		{R0-R7, PC}
 
 Hook1:
     PUSH  {R0-R1,R4,LR}
@@ -61,221 +64,254 @@ Hook1:
     LDR   R2, =ADDR_RECORD1
     BLX   R2
     MOV   R0, R4
-    MOV   R1, #0xD
-    LDR   R2, =ADDR_NUMX
-    BLX   R2
+    BL    AddNewLine
     POP   {R0-R1,R4}
     BL    AppendInfoW	
     POP   {PC}
 
-Hook2:
 #ifdef NEWSGOLD
-    PUSH	{LR}
-    LDR		R0, =ADDR_IsCalling           //0xA0DE7EA4+1,10B5041C????????4121201C????????D4+22
-    BLX		R0
-    MOV		R1, R0
+Hook2:
+	PUSH		{LR}
+	LDR		R0, =ADDR_IsCalling      //0xA0DE7EA4+1,10B5041C????????4121201C????????D4+22
+	BLX		R0
+	MOV		R1, R0
 #ifdef ELKA
-    MOV		R0, #1
+	MOV		R0, #1
 #else
-    MOV		R0, #0
+	MOV		R0, #0
 #endif
-    MOV		R2, R4
-    BL		UpdateLocaleToItem
-    LDR		R0, =ADDR_CallIN
-    BLX		R0		
-    POP		{PC}
-#else
-    MOV		R0, #0
-    MOV		R1, R4
-    MOV		R2, R5
-    BL		UpdateLocaleToItem
-    LDR		R0, =ADDR_CallIN
-    BLX		R0		
-#endif
+	MOV		R2, R4
+	BL		UpdateLocaleToItem
+	LDR		R0, =ADDR_CallIN
+	BLX		R0		
+	POP		{PC}
+
 
 Hook3:
-#ifdef NEWSGOLD
-    LDR		R0, =ADDR_IsCalling
-    BLX		R0
-    MOV		R1, R0
+	LDR		R0, =ADDR_IsCalling
+	BLX		R0
+	MOV		R1, R0
 #ifdef ELKA
-    MOV		R0, #4
+	MOV		R0, #4
 #else
-    MOV		R0, #3
+	MOV		R0, #3
 #endif
-    MOV		R2, R4
-#else
-    LDR		R0, =ADDR_CALLX
-    BLX		R0
-    MOV		R1, R0
-    MOV		R0, #3
-    MOV		R2, R4
-#endif
-    BL		UpdateLocaleToItem
-    LDR		R0, =ADDR_CallOUT
-    BLX		R0
+	MOV		R2, R4
+	BL		UpdateLocaleToItem
+	LDR		R0, =ADDR_CallOUT
+	BLX		R0
 
 Hook4:
-#ifdef NEWSGOLD
 #ifndef ELKA
-		ADD		R1, SP, #4
+	ADD		R1, SP, #4
 #endif
-#endif
-    PUSH	{LR}
-    MOV		R4, R0
-    LDR		R2, =ADDR_ADDRBook
-    BLX		R2
-    MOV		R0, R4
-    MOV		R1, #0xD
-    LDR		R2, =ADDR_NUMX
-    BLX		R2
-    MOV		R0, R4
-    MOV		R1, R4
-    BL		AppendInfoW
-#ifdef NEWSGOLD
+	PUSH		{LR}
+	MOV		R4, R0
+	LDR		R2, =ADDR_ADDRBook
+	BLX		R2
+	MOV		R0, R4
+	BL		AddNewLine
+	MOV		R1, R4
+	BL		AppendInfoW
 #ifdef ELKA
-    POP		{PC}
+	POP		{PC}
 #else
-    POP		{R2}
-    ADD		R2, #4
-    BX		R2
+	POP		{R2}
+	ADD		R2, #4
+	BX		R2
 #endif
-#else
-    POP		{PC}
-#endif
+
 		
 		
-#ifdef NEWSGOLD
+
 SIMBOOK:
 #ifdef ELKA
-		PUSH	{LR}
-		ADD		R1, R4, #0
-		ADD		R0, R6, #0
-		LDR		R3, =ADDR_GetPhoneBookNum
-		BLX		R3
-		MOV		R0,R4
-		MOV		R1,R4
-		BL		do_phonebook_work
-		POP		{R3}
-		ADD		R3, #4
-		BX		R3
+	PUSH		{LR}
+	ADD		R1, R4, #0
+	ADD		R0, R6, #0
+	LDR		R3, =ADDR_GetPhoneBookNum
+	BLX		R3
+	MOV		R0,R4
+	MOV		R1,R4
+	BL		AddNewLine
+	BL		AppendInfoW
+	POP		{R3}
+	ADD		R3, #4
+	BX		R3
 #else
-		PUSH	{LR}
-		MOV		R0,R4
-		MOV		R1,R4
-		BL		do_phonebook_work
-		MOV		R3, #3
-		POP		{PC}
+	PUSH		{LR}
+	MOV		R0,R4
+	MOV		R1,R4
+	BL		AddNewLine
+	BL		AppendInfoW
+	MOV		R3, #3
+	POP		{PC}
 #endif
 
-#else
-Hook5:
-    PUSH	{LR}
-    LDR		R0, [SP,#0x174]
-    MOV		R1, #0xA
-    LDR		R2, =ADDR_NUMX
-    BLX		R2
-    LDR		R0, [SP,#0x174]
-    LDR		R1, [SP,#0x1D0]
-    LDR		R2, =AppendInfoW
-    BLX		R2
-    MOV		R1, #0
-    ADD		R0, R7, #0
-    POP		{PC}
-#endif
-
-
-#ifdef NEWSGOLD
-    RSEG  HOOK_DUMP
-    CODE16
+	RSEG	HOOK_DUMP
+	CODE16
 HOOKRecoedWindow_DUMP:
-    PUSH  {R7,LR}	
-    LDR   R7, =Hook1
-    BLX   R7
-    POP   {R7,PC}
+	PUSH		{R7,LR}	
+	LDR		R7, =Hook1
+	BLX		R7
+	POP		{R7,PC}
 HOOKCallinwindow_DUMP:
-    LDR   R7, =Hook2
-    BX    R7
+	LDR		R7, =Hook2
+	BX		R7
 HOOKCallOutWindow_DUMP:
-    LDR   R7, =Hook3
-    BLX   R7
-    
+	LDR		R7, =Hook3
+	BLX		R7
+	
 #ifdef ELKA
 HOOKAddrBookWindow_DUMP:
-    PUSH  {R7,LR}	
-    LDR   R7, =Hook4
-    BLX   R7
-    POP   {R7,PC}
+	PUSH		{R7,LR}	
+	LDR		R7, =Hook4
+	BLX		R7
+	POP		{R7,PC}
 #endif
 
-    RSEG  RecordWindow:CODE(1)
-    CODE16
-    BL    HOOKRecoedWindow_DUMP
-    
-    RSEG Callinwindow:CODE(1)
-    CODE16
-    BL    HOOKCallinwindow_DUMP
-    
-    
-    RSEG  CallOutWindow:CODE(1)
-    CODE16
-    BL    HOOKCallOutWindow_DUMP
+	RSEG	RecordWindow:CODE(1)
+	CODE16
+	BL		HOOKRecoedWindow_DUMP
+	
+	RSEG	Callinwindow:CODE(1)
+	CODE16
+	BL		HOOKCallinwindow_DUMP
+	
+	
+	RSEG	CallOutWindow:CODE(1)
+	CODE16
+	BL		HOOKCallOutWindow_DUMP
 
 #ifdef ELKA
-    RSEG  AddrBookWindow:CODE(1)
-    CODE16
-    BL    HOOKAddrBookWindow_DUMP
+	RSEG	AddrBookWindow:CODE(1)
+	CODE16
+	BL		HOOKAddrBookWindow_DUMP
 #else
-    RSEG  AddrBookWindow
-    CODE32
-    LDR		R4, =Hook4
-    BLX		R4
+	RSEG	AddrBookWindow
+	CODE32
+	LDR		R4, =Hook4
+	BLX		R4
 #endif
 #ifdef ELKA
-		CODE16
-		RSEG	PHONEBOOKHOOK:CODE(2)
-		LDR		R1, =SIMBOOK
-		BLX		R1
+	CODE16
+	RSEG	PHONEBOOKHOOK:CODE(2)
+	LDR		R1, =SIMBOOK
+	BLX		R1
 #else
-		CODE32
-		RSEG	PHONEBOOKHOOK
-		BLX		SIMBOOK
+	CODE32
+	RSEG	PHONEBOOKHOOK
+	BLX		SIMBOOK
 #endif	
 
 #ifndef ELKA
-    RSEG  AddrBookWindow2:DATA(1)
-    DCB		0xFF
+	RSEG  AddrBookWindow2:DATA(1)
+	DCB		0xFF
 #endif
 
 #else
+//SGOLD
+
+Hook2:
+	MOV		R0, #0
+	MOV		R1, R4
+	MOV		R2, R5
+	BL		UpdateLocaleToItem
+	LDR		R0, =ADDR_CallIN
+	BLX		R0		
+
+Hook3:
+	LDR		R0, =ADDR_CALLX
+	BLX		R0
+	MOV		R1, R0
+	MOV		R0, #3
+	MOV		R2, R4
+	BL		UpdateLocaleToItem
+	LDR		R0, =ADDR_CallOUT
+	BLX		R0
+
+Hook4:
+	PUSH		{LR}
+	MOV		R4, R0
+	LDR		R2, =ADDR_ADDRBook
+	BLX		R2
+	MOV		R0, R4
+	BL		AddNewLine
+	MOV		R1, R4
+	BL		AppendInfoW
+	POP		{PC}
+
+Hook5:
+	PUSH		{LR}
+	LDR		R0, [SP,#0x174]
+	BL		AddNewLine
+	LDR		R1, [SP,#0x1D0]
+	BL		AppendInfoW
+	MOV		R1, #0
+	ADD		R0, R7, #0
+	POP		{PC}
+
+//短信发送动画窗口,by BingK(binghelingxi)
+	EXTERN		GetProvAndCity
+SMS_SEND_WINDOW:
+	PUSH		{R4, LR}
+	MOV		R2, #0
+	ADD		R1, R7, #0
+	ADD		R0, R5, #0
+	LDR		R3, =ADDR_SMS_SEND
+	BLX		R3
+	MOV		R0, R5
+	BL		AddNewLine
+	LDR		R0, [R5, #0]
+	POP		{R4}
+	MOV		R3, #0xBC
+	LDR		R1, [R4, R3]
+	BL		GetProvAndCity
+	POP		{R0}
+	ADD		R0, R0, #4
+	BX		R0
+
 //Hook
 // 通话记录修改
-    RSEG  RecordWindow:CODE(1)
-    CODE16
-    BL      Hook1
+	RSEG	RecordWindow:CODE(1)
+	CODE16
+	BL		Hook1
 
 // 拨入窗口修改，覆盖了group xx的显示位置
-    RSEG Callinwindow:CODE(1)
-    CODE16
-    BL      Hook2
+	RSEG	Callinwindow:CODE(1)
+	CODE16
+	BL		Hook2
 
 // 拨出窗口修改，覆盖了group xx的显示位置
-    RSEG  CallOutWindow:CODE(1)
-    CODE16
-    BL      Hook3
+	RSEG	CallOutWindow:CODE(1)
+	CODE16
+	BL		Hook3
 
 // 通讯录窗口修改
-    RSEG  AddrBookWindow:CODE(1)
-    CODE16
-    BL      Hook4
+	RSEG 	AddrBookWindow:CODE(1)
+	CODE16
+	BL		Hook4
 
-    RSEG	AddrBookWindow2:DATA(1)
-    DCB		0x21
+	RSEG	AddrBookWindow2:DATA(1)
+	DCB		0x21
 
 // 电话本窗口修改
-    RSEG	PhonebookWindow:CODE(1)
-    CODE16
-    BL      Hook5
-#endif    
+	RSEG	PhonebookWindow:CODE(1)
+	CODE16
+	BL		Hook5
 
-		END
+//短信发送动画窗口,by BingK(binghelingxi)	
+	CODE16
+	RSEG	SMS_SEND_HOOK:CODE(2)
+	LDR		R2, pSMS_SEND
+	BLX		R2
+	
+	DATA
+pSMS_SEND
+	DCD		SMS_SEND_WINDOW
+	
+	CODE16
+	NOP
+	
+#endif
+	END
