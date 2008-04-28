2e841e02aa53ad1117e36988e44196f5e5611856
bingkx <bingkx>
2008-04-28 12:19:47 +0000

    增加中键撤销功能 增加长按快速加减，一次为3 优化代码

--- SMSCountReader/main.c
+++ SMSCountReader/main.c
@@ -1,6 +1,5 @@
 #include "..\inc\swilib.h"
 
-#define CP_MSG "SMSCountReader v1.6\n(C) 2008 BingK(binghelingxi)"
 
 typedef struct
 {
@@ -23,6 +22,33 @@ int screenw;
 int screenh;
 char dat_path[]="2:\\SMSCount.dat";
 int f;
+unsigned int ferr;
+WSHDR *ws_sk;
+char *utf8_str;
+WSHDR *xws;
+
+#define CP_MSG		"SMSCountReader v1.7\n(C) 2008 BingK(binghelingxi)"
+#define ELFNAME		"SMSCountReader"
+#define szLoadDATFail	"Load SMSCount.dat failed!"
+#define szSaveALL	"Save All Change?"
+#define szClearALL	"Clear ALL?"
+#define szSaveErr	"Save Error!"
+#define szEditExit	"Is Editing, Save First?"
+#define utf8_clear	"\xE6\xB8\x85\xE9\x9B\xB6" //清零
+#define utf8_exit	"\xE9\x80\x80\xE5\x87\xBA" //退出
+#define utf8_edit	"\xE7\xBC\x96\xE8\xBE\x91" //编辑
+#define utf8_cancel	"\xE6\x92\xA4\xE9\x94\x80" //撤销
+#define name_header	"SMSCount v3.7.1\nSMSCountReader v1.7\n" 
+#define utf8_all	"\xE5\x85\xA8\xE9\x83\xA8: %d" //全部
+#define utf8_chm	"\xE7\xA7\xBB\xE5\x8A\xA8: %d"  //移动
+#define utf8_chu	"\xE8\x81\x94\xE9\x80\x9A: %d"  //联通
+#define utf8_xlt	"\xE5\xB0\x8F\xE7\x81\xB5\xE9\x80\x9A: %d"  //小灵通
+#define utf8_oth	"\xE5\x85\xB6\xE4\xBB\x96: %d"  //其它
+#define utf8_date	"\xE5\xBD\x93\xE5\x89\x8D\xE6\x97\xA5\xE6\x9C\x9F: %04d.%i.%i" 
+#define utf8_editing	"\xE7\xBC\x96\xE8\xBE\x91\xE4\xB8\xAD\xE3\x80\x82\xE3\x80\x82\xE3\x80\x82\n\xE8\xAF\xB7\xE6\x8C\x89\xE7\xBB\xBF\xE9\x94\xAE\xE4\xBF\x9D\xE5\xAD\x98" 
+#define utf8_press_enter_key	"\xE6\x8C\x89\xE4\xB8\xAD\xE9\x94\xAE\xE5\xBC\x80\xE5\xA7\x8B\xE4\xBF\xAE\xE6\x94\xB9\xE6\x95\xB0\xE6\x8D\xAE" 
+
+#define FASTADD	  3
 
 #define	COUNT_ALL 0
 #define	COUNT_CHM 1
@@ -32,9 +58,17 @@ int f;
 #define	TYPE_ALL  5
 #define	BUF_LEN   (TYPE_ALL*sizeof(int))
 
-int count[TYPE_ALL];
 
-#pragma inline
+int count[TYPE_ALL];
+int count_bak[TYPE_ALL];
+
+void intmemcpy_n(int *s1, int *s2, int len)
+{
+	int i;
+	for(i=0;i<len;i++)
+		s1[i]=s2[i];
+}
+
 int get_string_width(WSHDR *ws, int font)
 {
   int width=0;
@@ -56,30 +90,33 @@ void patch_rect(RECT*rc,int x,int y, int x2, int y2)
 	rc->x2=x2;
 	rc->y2=y2;
 }
+
 int load_dat(void)
 {
-	unsigned int err;
-	if((f=fopen(dat_path, A_ReadWrite+A_BIN, P_READ+P_WRITE, &err))!=-1)
+	//unsigned int err;
+	if((f=fopen(dat_path, A_ReadWrite+A_BIN, P_READ+P_WRITE, &ferr))!=-1)
 	{
-		fread(f, count, BUF_LEN, &err);
-		lseek(f, 0, S_SET, &err, &err);
+		fread(f, count, BUF_LEN, &ferr);
+		lseek(f, 0, S_SET, &ferr, &ferr);
+		intmemcpy_n(count_bak, count, TYPE_ALL);
 	}
-	else if((f=fopen(dat_path, A_ReadWrite+A_Create+A_Truncate, P_READ+P_WRITE, &err))==-1)
+	else if((f=fopen(dat_path, A_ReadWrite+A_Create+A_Truncate, P_READ+P_WRITE, &ferr))==-1)
 		return -1;
 	return 1;
 }
 
 void save_dat(int type)
 {
-	unsigned int err;
+	//unsigned int err;
 	if(type==1)
 	{
 		int i;
 		for(i=0; i<TYPE_ALL; i++)
 			count[i]=0;
 	}
-	if(fwrite(f, count, BUF_LEN, &err)!=BUF_LEN)
-		MsgBoxError(1,(int)"Save Error");
+	if(fwrite(f, count, BUF_LEN, &ferr)!=BUF_LEN)
+		MsgBoxError(1,(int)szSaveErr);
+	intmemcpy_n(count_bak, count, TYPE_ALL);
 	is_editing=0;
 }
 void clearProc(int id)
@@ -94,54 +131,64 @@ void saveProc(int id)
     save_dat(0);
 }
 
+void exitsaveProc(int id)
+{
+	if(id==0)
+		save_dat(0);
+	CloseCSM(MAINCSM_ID);
+}
+
 void exit(void)
 {
 	if(is_editing)
 	{
-		MsgBoxYesNo(1, (int)"Is Editing, Save First?", saveProc);
+		MsgBoxYesNo(1, (int)szEditExit, exitsaveProc);
 	}
 	else
 		CloseCSM(MAINCSM_ID);
 }
 
+void cancel_edit(void)
+{
+	intmemcpy_n(count, count_bak, TYPE_ALL);
+	is_editing=0;
+}
 
 void soft_key(void)
 {
-  char utf8_clear[]="\xE6\xB8\x85\xE7\xA9\xBA\xE8\xAE\xA1\xE6\x95\xB0";//清空计数
-  char utf8_exit[]="\xE9\x80\x80\xE5\x87\xBA";//退出
-  WSHDR *wsl=AllocWS(16);
-  WSHDR *wsr=AllocWS(16);
-  utf8_2ws(wsr,utf8_exit,strlen(utf8_exit));
-  utf8_2ws(wsl,utf8_clear,strlen(utf8_clear));
-  DrawString(wsr,screenw-get_string_width(wsr,FONT_MEDIUM)-4,screenh-GetFontYSIZE(FONT_MEDIUM)-2,screenw,screenh,FONT_MEDIUM,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
-  DrawString(wsl,2,screenh-GetFontYSIZE(FONT_MEDIUM)-2,screenw,screenh,FONT_MEDIUM,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
-  FreeWS(wsr);
-  FreeWS(wsl);
+  //WSHDR *wsl=AllocWS(32);
+  utf8_2ws(ws_sk,utf8_exit,strlen(utf8_exit));
+  DrawString(ws_sk,screenw-get_string_width(ws_sk,FONT_SMALL)-4,screenh-GetFontYSIZE(FONT_SMALL)-2,screenw,screenh,FONT_SMALL,TEXT_OUTLINE+TEXT_ALIGNRIGHT,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+  utf8_2ws(ws_sk,utf8_clear,strlen(utf8_clear));
+  DrawString(ws_sk,2,screenh-GetFontYSIZE(FONT_SMALL)-2,screenw,screenh,FONT_SMALL,TEXT_OUTLINE+TEXT_ALIGNLEFT,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+  if(is_editing)
+  	utf8_2ws(ws_sk,utf8_cancel,strlen(utf8_cancel));
+  else
+  	utf8_2ws(ws_sk,utf8_edit,strlen(utf8_edit));
+  DrawString(ws_sk,screenw/2-get_string_width(ws_sk,FONT_SMALL)/2-0x10,screenh-GetFontYSIZE(FONT_SMALL)-2,screenw/2+get_string_width(ws_sk,FONT_SMALL)/2+0x10,screenh,FONT_SMALL,TEXT_OUTLINE+TEXT_ALIGNMIDDLE,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+  //FreeWS(wsl);
 }
 
 void onRedraw(MAIN_GUI *data)
 {
 #ifdef ELKA
-  DisableIconBar(1);
+	DisableIconBar(1);
 #endif
-  int color_index=0;
-	//int init_day;
-	//if(no_bcfg) init_day=1;
-	//  else init_day=get_initday();
+	int color_index=0;
 	int font_h=GetFontYSIZE(FONT_SMALL);
 	DrawRectangle(0,0,screenw,screenh,0,GetPaletteAdrByColorIndex(1),GetPaletteAdrByColorIndex(1));
 	soft_key();
-	char *utf8_str=malloc(128);
-	WSHDR *ws = AllocWS(128);
-	wsprintf(ws, "SMSCount v3.7.1\nSMSCountReader v1.6\n");
-	DrawString(ws,0,0,screenw,screenh,FONT_SMALL,TEXT_ALIGNMIDDLE+TEXT_OUTLINE,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+//	char *utf8_str=malloc(128);
+//	WSHDR *ws = AllocWS(128);
+	wsprintf(xws, name_header);
+	DrawString(xws,0,0,screenw,screenh,FONT_SMALL,TEXT_ALIGNMIDDLE+TEXT_OUTLINE,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
 	//全部
-	sprintf(utf8_str,"\xE5\x85\xA8\xE9\x83\xA8: %d", count[0]);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
-	DrawString(ws,0,3*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
+	sprintf(utf8_str, utf8_all, count[0]);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
+	DrawString(xws,2,3*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 
 	//移动
-	sprintf(utf8_str,"\xE7\xA7\xBB\xE5\x8A\xA8: %d", count[1]);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
+	sprintf(utf8_str, utf8_chm, count[1]);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
 	if(is_editing)
 	{
 	  if(edit_line==1)
@@ -149,10 +196,10 @@ void onRedraw(MAIN_GUI *data)
 	  else color_index=0;
 	}
 	else color_index=0;
-	DrawString(ws,0,4*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
+	DrawString(xws,2,4*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
 	//联通
-	sprintf(utf8_str,"\xE8\x81\x94\xE9\x80\x9A: %d", count[2]);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
+	sprintf(utf8_str, utf8_chu, count[2]);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
 	if(is_editing)
 	{
 	  if(edit_line==2)
@@ -160,10 +207,10 @@ void onRedraw(MAIN_GUI *data)
 	  else color_index=0;
 	}
 	else color_index=0;
-	DrawString(ws,0,5*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
+	DrawString(xws,2,5*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
 	//小灵通
-	sprintf(utf8_str,"\xE5\xB0\x8F\xE7\x81\xB5\xE9\x80\x9A: %d", count[3]);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
+	sprintf(utf8_str, utf8_xlt, count[3]);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
 	if(is_editing)
 	{
 	  if(edit_line==3)
@@ -171,10 +218,10 @@ void onRedraw(MAIN_GUI *data)
 	  else color_index=0;
 	}
 	else color_index=0;
-	DrawString(ws,0,6*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
+	DrawString(xws,2,6*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
 	//其它
-	sprintf(utf8_str,"\xE5\x85\xB6\xE4\xBB\x96: %d", count[4]);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
+	sprintf(utf8_str, utf8_oth, count[4]);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
 	if(is_editing)
 	{
 	  if(edit_line==4)
@@ -182,38 +229,23 @@ void onRedraw(MAIN_GUI *data)
 	  else color_index=0;
 	}
 	else color_index=0;
-	DrawString(ws,0,7*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
-	//清零日期
-  //if(init_day==0)
-  //{
-  //  sprintf(utf8_str,"\xE6\xB8\x85\xE9\x9B\xB6\xE6\x97\xA5\xE6\x9C\x9F: \xE4\xB8\x8D\xE6\xB8\x85\xE9\x9B\xB6");
-  //}
-  //else sprintf(utf8_str,"\xE6\xB8\x85\xE9\x9B\xB6\xE6\x97\xA5\xE6\x9C\x9F: \xE6\xAF\x8F\xE6\x9C\x88%d\xE6\x97\xA5",init_day);
-//	utf8_2ws(ws,utf8_str,strlen(utf8_str));
-//	DrawString(ws,0,8*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23)); 	
+	DrawString(xws,2,7*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(color_index),GetPaletteAdrByColorIndex(23)); 
 	//显示当前日期
 	TTime tt;
 	TDate td;
 	GetDateTime(&td,&tt);
-	sprintf(utf8_str,"\xE5\xBD\x93\xE5\x89\x8D\xE6\x97\xA5\xE6\x9C\x9F: %04d.%i.%i",td.year,td.month,td.day);
-	utf8_2ws(ws,utf8_str,strlen(utf8_str));
-	DrawString(ws,0,9*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23));
+	sprintf(utf8_str, utf8_date, td.year, td.month, td.day);
+	utf8_2ws(xws,utf8_str,strlen(utf8_str));
+	DrawString(xws,2,9*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(0),GetPaletteAdrByColorIndex(23));
 	//正在编辑 
 	if(is_editing)
-	{
-		sprintf(utf8_str,"\xE7\xBC\x96\xE8\xBE\x91\xE4\xB8\xAD\xE3\x80\x82\xE3\x80\x82\xE3\x80\x82\n\xE8\xAF\xB7\xE6\x8C\x89\xE7\xBB\xBF\xE9\x94\xAE\xE4\xBF\x9D\xE5\xAD\x98");
-		utf8_2ws(ws,utf8_str,strlen(utf8_str));
-		DrawString(ws,0,10*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(7),GetPaletteAdrByColorIndex(23)); 
-	}
+		utf8_2ws(xws,utf8_editing,strlen(utf8_editing));
 	else //提示按中键编辑
-  	{
-  		sprintf(utf8_str,"\xE6\x8C\x89\xE4\xB8\xAD\xE9\x94\xAE\xE5\xBC\x80\xE5\xA7\x8B\xE4\xBF\xAE\xE6\x94\xB9\xE6\x95\xB0\xE6\x8D\xAE");
-		utf8_2ws(ws,utf8_str,strlen(utf8_str));
-		DrawString(ws,0,10*font_h,screenw,screenh,FONT_SMALL,32,GetPaletteAdrByColorIndex(7),GetPaletteAdrByColorIndex(23)); 
-	}
+		utf8_2ws(xws, utf8_press_enter_key, strlen(utf8_press_enter_key));
+	DrawString(xws,2,10*font_h,screenw,screenh,FONT_SMALL,TEXT_OUTLINE,GetPaletteAdrByColorIndex(7),GetPaletteAdrByColorIndex(23)); 
   
-	mfree(utf8_str);
-	FreeWS(ws);
+//	mfree(utf8_str);
+//	FreeWS(ws);
 }
 
 void show_about(void)
@@ -223,7 +255,7 @@ void show_about(void)
 
 int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 {
-	if(msg->gbsmsg->msg==KEY_UP)
+	if(msg->gbsmsg->msg==KEY_DOWN)
 	{
 	  int i=msg->gbsmsg->submess;
 	  switch(i)
@@ -232,12 +264,13 @@ int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 	      exit();
 	      break;
 	    case LEFT_SOFT:
-	      MsgBoxYesNo(1, (int)"Clear ALL?", clearProc);
-	      REDRAW();
+	      MsgBoxYesNo(1, (int)szClearALL, clearProc);
 	      break;
 	    case ENTER_BUTTON:
-	      is_editing=1;
-	      REDRAW();
+	    	if(is_editing==0)
+	      		is_editing=1;
+	      	else
+	      		cancel_edit();
 	      break;
 	    case DOWN_BUTTON:
 	    	if(is_editing)
@@ -246,7 +279,6 @@ int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 	        edit_line++;
 	      else edit_line=1;
 	      	}
-	      REDRAW();
 	      break;
 	    case UP_BUTTON:
 	    	if(is_editing)
@@ -255,7 +287,6 @@ int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 	        edit_line--;
 	      else edit_line=4;
 		}
-	      REDRAW();
 	      break;
 	    case LEFT_BUTTON:
 	    	if(is_editing)
@@ -266,7 +297,6 @@ int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 	        count[edit_line]--;
 	      }
 		}
-	      REDRAW();
 	      break;
 	    case RIGHT_BUTTON:
 	    	if(is_editing)
@@ -274,17 +304,58 @@ int OnKey(MAIN_GUI *data, GUI_MSG *msg)
 	        count[0]++;
 	        count[edit_line]++;
 		}
-	      REDRAW();
 	      break;
 	    case GREEN_BUTTON:
-	      if(is_editing) MsgBoxYesNo(1, (int)"Save All Change?", saveProc);
-	      REDRAW();
+	      if(is_editing) MsgBoxYesNo(1, (int)szSaveALL, saveProc);
 	      break;
 	     case '*':
 	     	show_about();
 	     	break; 
 	  }
 	}
+	if(msg->gbsmsg->msg==LONG_PRESS)
+	{
+		int i=msg->gbsmsg->submess;
+		switch(i)
+		{
+		case DOWN_BUTTON:
+	    		if(is_editing)
+	    		{
+				if(edit_line!=4) 
+					edit_line++;
+				else
+					edit_line=1;
+			}
+			break;
+		case UP_BUTTON:
+			if(is_editing)
+			{
+				if(edit_line!=1)
+					edit_line--;
+				else 
+					edit_line=4;
+			}
+			break;
+		case LEFT_BUTTON:
+			if(is_editing)
+			{
+				if(count[edit_line]>FASTADD)
+				{
+					count[0]-=FASTADD;
+					count[edit_line]-=FASTADD;
+				}
+			}
+			break;
+		case RIGHT_BUTTON:
+			if(is_editing)
+			{
+				count[0]+=FASTADD;
+				count[edit_line]+=FASTADD;
+			}
+			break;
+		}
+	}
+	DirectRedrawGUI();
 	return(0);
 }
 
@@ -333,11 +404,14 @@ void maincsm_oncreate(CSM_RAM *data)
 {
 	screenw=ScreenW()-1;
 	screenh=ScreenH()-1;
+	ws_sk=AllocWS(32);
+	utf8_str=malloc(256);
+	xws=AllocWS(256);
 	int i;
 	for(i=0; i<TYPE_ALL; i++)
 		count[i]=0;
 	if(load_dat()<0)
-		ShowMSG(1, (int)"Load SMSCount.dat failed");
+		ShowMSG(1, (int)szLoadDATFail);
 	MAIN_GUI *main_gui=malloc(sizeof(MAIN_GUI));
 	MAIN_CSM *csm=(MAIN_CSM *)data;
 	zeromem(main_gui,sizeof(MAIN_GUI));
@@ -347,11 +421,11 @@ void maincsm_oncreate(CSM_RAM *data)
 	main_gui->gui.methods=(void *)gui_methods;
 	main_gui->gui.item_ll.data_mfree=(void (*)(void *))mfree_adr();	
 	
-  csm->csm.state=0;
-  csm->csm.unk1=0;
-  csm->gui_id=CreateGUI(main_gui);
-  
-  MAINGUI_ID=csm->gui_id;
+	csm->csm.state=0;
+	csm->csm.unk1=0;
+	csm->gui_id=CreateGUI(main_gui);
+	
+	MAINGUI_ID=csm->gui_id;
 }
 
 int maincsm_onmessage(CSM_RAM *data, GBS_MSG *msg)
@@ -367,8 +441,11 @@ int maincsm_onmessage(CSM_RAM *data, GBS_MSG *msg)
 void Killer(void)
 {
   extern void *ELF_BEGIN;
-  unsigned int err;
-  fclose(f, &err);
+  //unsigned int err;
+  fclose(f, &ferr);
+  FreeWS(ws_sk);
+  mfree(utf8_str);
+  FreeWS(xws);
   kill_data(&ELF_BEGIN,(void (*)(void *))mfree_adr());
 }
 
@@ -411,7 +488,7 @@ sizeof(MAIN_CSM),
 
 void UpdateCSMname(void) 
 {
-  wsprintf((WSHDR *)(&MAINCSM.maincsm_name), "SMSCountReader");
+  wsprintf((WSHDR *)(&MAINCSM.maincsm_name), ELFNAME);
 }
 
 int main(char *initday)
