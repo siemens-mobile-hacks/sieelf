b21247ed7c2d1b14e1a0d2e29cdac2ca87a0b66d
bingkx <bingkx>
2008-07-10 01:39:38 +0000

    ;v1.2 ;等待输入状态: 1-进入选择 ;输入拼音状态: 1-进入选择,5-直接选择第一个字,9-下,*-左,0-右 ;选择拼音状态: 9-下,*-左,0-右

--- IME_PINYIN/addr.h
+++ IME_PINYIN/addr.h
@@ -2,6 +2,7 @@
 #define GBS_SendMessage_adr	0xA0091EAB
 #define DO_IME_BACK		0xA0B1F172+1 //
 #define DO_RIGHT		0xA0B1F37E+1 //输入状态已检测
+#define	DO_LEFT			0xA0B1F314+1
 #define	DO_DOWN			0xA0B1F45C+1 //输入状态已检测
 #define	DO_SELECT		0xA0B1F248+1 //
 #define DO_DIRECT_SELECT	0xA0B1F6D0+1 //只能在非JAVA中使用
@@ -9,5 +10,10 @@
 #define	DO_KEY1_BACK		0xA0B1F77C+1 //1键返回
 #define	JAVA_EDIT_KOP		0xA8EB0A30   //在JAVA中,这个地址不变
 #define	USE_RAM			0xA8D805C0   //4字节,用于存储JAVA中输入法选择菜单状态
+#define DO_XING_FIX		0xA06EEAE8+1 //解决*键在JAVA中选字状态向右无用问题
+#define	GET_INPUT_STATE		0xA06EF026+1 //获取输入状态,1为等待输入,0为拼音选择或选字状态
+#define	KEY0_FUNC1		0xA06EDDC6+1 //0键跳转位置的一个函数,返回时要先执行这个函数
+#define	KEY0_BACK		0xA06EF1DC+1 //0键跳转返回地址
+#define	KEY0_GOTO_IME		0xA06EF1EC+1 //0键跳转向常规输入
 #endif
 
