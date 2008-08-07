2ea328e3ce858e27be86be55b83c97ed284d34fb
bingkx <bingkx>
2008-08-07 15:11:25 +0000

    获取来短信号码部分,修正

--- CodeShowPatch/sms_num.c
+++ CodeShowPatch/sms_num.c
@@ -1,55 +1,66 @@
 #include "addr.h"
 
-__thumb void Hex2Num(char *hex, char *num, int len) @ "CODE_X"
+__thumb int Hex2Num(char *hex, char *num, int len) @ "CODE_X" //返回长度
 {
 	int c;
-	int i=0,j;
+	int i=0,j=0;
 	char *p=hex;
 	if(*p==0x91)
 	{
-		//num[i]='+';
-		//i++;
 		p++;
+		len-=2; //按+86算
 	}
 	p++;
-	for (j=0; j<len; j++)
+	while(i<len)
 	{
 		c=p[j]%0x10;
 		if(c>=0&&c<=9)
-			num[i]=c+'0';
+			num[i++]=c+'0';
 		else
 			break;
-		i++;
+		if(i>=len)
+			break;
 		c=p[j]/0x10;
 		if(c>=0&&c<=9)
-			num[i]=c+'0';
+			num[i++]=c+'0';
 		else
 			break;
-		i++;
+		j++;
 	}
 	num[i]=0;
+	return i;
 }
 
 __thumb int GetNumFromIncomingPDU(char *num) @ "CODE_X"
-{
+{	
 	int c;
+	int i;
 	char *p=(char *)ADDR_IncommingPDU;
 	if ( *p++ != 0x11 )
 		return 0;
 	if ( *p++ != 0x11 )
 		return 0;
-	p++;
-	c=*p++;
+	if(!(*p++))
+		return 0;
+	c=*p++; //信息中心长度
 	p+=c+1;
-	c=*p++;
+	if(p[1]!=0x91&&p[1]!=0xA1) //常规短信
+	{
+		for(i=1;i<5;i++) //查找下面4字节
+		{
+			if(p[i]==0x91||p[i]==0xA1)
+			{
+				p+=i-1; //走到长度位置
+				goto GET_L;
+			}
+		}
+		return 0;//未知情况直接放弃
+	}
+GET_L:
+	c=*p++; //号码长度
 	if(c)
 	{
-		if(c%2)
-			c=c/2+2;
-		else
-			c=c/2+1;
-		Hex2Num(p, num, c);
-		return 1;
+		return(Hex2Num(p, num, c));
 	}
 	return 0;
 }
