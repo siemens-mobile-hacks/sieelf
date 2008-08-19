4c6cc975b392e838de1dabd451827350c06f8200
snoopyhzy <snoopyhzy>
2008-08-19 07:11:16 +0000

    ELC1v41

--- IME_BIHUA/addr.h
+++ IME_BIHUA/addr.h
@@ -22,3 +22,27 @@
 #define	KEY0_GOTO_IME		0xA06EF1EC+1 //0键跳转向常规输入
 
 #endif
+
+#ifdef ELC1sw41
+#define	DO_DIRECT_SELECT	0xA0B28D6E+1
+#define	DO_START_SELECT		0xA0B289C4+1
+#define	DO_EBACK		0xA0B28880+1
+#define	DO_NOTHING		0xA0B27BA2+1
+#define	WGET_KBS_KSTATE		0xA0A50736+1 //1:ON, 0:OFF
+#define	UNK_RAM_adr		0xA8EBC98C //可用于判断是否处于编辑状态
+#define	WSET_KBS_KSTATE		0xA0A50498+1
+#define	DO_SELECT_EBACK		0xA0B28F00+1
+#define	KDB_KPOP_FUNC1_RAM	0xA8F2B658
+#define	KDB_KPOP_EBACK		0xA06F6BBC+1
+#define	KDB_KPOP_EBNE_adr	0xA06F6C54+1
+
+/* 以下部分和拼音输入法修改 v1.x 中的一样 */
+#define GBS_SendMessage_adr	0xA0091EAB
+#define	JAVA_EDIT_KOP		0xA8EB0730   //在JAVA中,这个地址不变
+#define	USE_RAM			0xA8D805C0   //4字节,用于存储JAVA中输入法选择菜单状态
+#define	GET_INPUT_STATE		0xA06F6952+1 //获取输入状态,1为等待输入,0为拼音选择或选字状态
+#define	KEY0_FUNC1		0xA06F56F2+1 //0键跳转位置的一个函数,返回时要先执行这个函数
+#define	KEY0_BACK		0xA06F6B08+1 //0键跳转返回地址
+#define	KEY0_GOTO_IME		0xA06F6B18+1 //0键跳转向常规输入
+
+#endif
