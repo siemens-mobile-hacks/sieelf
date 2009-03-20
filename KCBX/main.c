f327cd914008a51cecdbdb10cf74003933f1e7bb
comerose@126.com <comerose@126.com>
2009-03-20 04:13:54 +0000

    ①调整配置文件中的默认参数! ②修正有时候闹钟不响的问题!

--- KCBX/main.c
+++ KCBX/main.c
@@ -21,10 +21,9 @@ extern const char BGM_COLOR[];//上午
 extern const char BGP_COLOR[];//下午
 extern const char BGN_COLOR[];//晚上
 
-static short point_x=1, point_y=1; //移动查看选中项
+static byte p_x=1, p_y=1; //移动查看选中项
 static char *Data;
-
-#define tsColor GetPaletteAdrByColorIndex(23)
+static byte  Kcb=6;
 
 typedef struct{
   CSM_RAM csm;
@@ -36,36 +35,36 @@ typedef struct{
 } MAIN_GUI;
 
 
-static const char ver[16] = "课程表v2.03";
+static const char ver[16] = "课程表v2.06";
 
-unsigned short daylist=0;
-unsigned int err;
-TDate d;
-TTime t;
+static byte dlt=0;
+static TDate d;
+static TTime t;
 
 unsigned int MAINCSM_ID = 0;
-static short ac=10;
+static byte ac=10;
 
 static void onRedraw(MAIN_GUI *data){
   WSHDR* ws = AllocWS(20);
-  static short LW,week,i,j;
+  static short LW,week;
+  static TRect rc;
 #ifdef ELKA
-  static short aa=2;
-  static short ad=24;
-  static short y1=28;
-  static short y2=24;
-  static short ww=29;  
+  static byte aa=2;
+  static byte ad=24;
+  static byte y1=28;
+  static byte y2=24;
+  static byte ww=29;  
 #else
-  static short aa=1;
-  static short ad=1;
-  static short y1=18;
-  static short y2=14;  
-  static short ww=16;  
+  static byte aa=1;
+  static byte ad=1;
+  static byte y1=18;
+  static byte y2=14;  
+  static byte ww=16;  
 #endif
-  short OHigh=y1+y2+ad;  
-  short Right=ww*8+aa+2;
-  short Left =aa+1;
-  short YSize =GetFontYSIZE(FONT_SMALL);
+  word OHigh=y1+y2+ad;  
+  word Right=ww*8+aa+2;
+  word Left =aa+1;
+  word YSize =GetFontYSIZE(FONT_SMALL);
   //时间日期星期
   GetDateTime(&d, &t);
   //计算周次
@@ -74,7 +73,7 @@ static void onRedraw(MAIN_GUI *data){
   DrawRectangle(0,YDISP,ScreenW()-1,ScreenH()-1,0,BGA_COLOR,BGA_COLOR);
   //课程表标题
   wsprintf(ws,PNT_ONE,ver);
-  DrawString(ws,-1,YDISP+2, ScreenW()-1,3+YDISP+YSize,8,2,FNT_COLOR,tsColor);
+  DrawString(ws,0,YDISP+2, ScreenW()-1,3+YDISP+YSize,8,2,FNT_COLOR,TRAN_CBK);
   //画周次时间信息
   WSHDR* wsL = AllocWS(4);
   wsAppendChar(wsL,0xE405);  
@@ -84,8 +83,7 @@ static void onRedraw(MAIN_GUI *data){
   }else{
      wsprintf(ws, "%w:%dW %02d-%02d %02d:%02dA",wsL,week,d.month,d.day,t.hour,t.min);
   }     
-  
-  DrawString(ws,-1,YDISP+YSize+6,ScreenW()-1,YDISP+YSize*2+6,FONT_SMALL_BOLD,2,FNT_COLOR,tsColor);
+  DrawString(ws,0,YDISP+YSize+6,ScreenW()-1,YDISP+YSize*2+6,FONT_SMALL_BOLD,2,FNT_COLOR,TRAN_CBK);
   FreeWS(wsL);
   //画背景框  1.时间；2.星期；3.上午；4.下午；5.晚上 width*2+aa
   DrawRectangle(Left,OHigh,      ww*2+aa+2,y1+ad+y2*11,0,BGT_COLOR,BGT_COLOR);  // 1.时间
@@ -94,21 +92,23 @@ static void onRedraw(MAIN_GUI *data){
   DrawRectangle(Left,y1+y2*6+ad, Right,    y1+ad+y2*ac,0,BGP_COLOR,BGP_COLOR); // 4.下午
   DrawRectangle(Left,y1+y2*ac+1, Right,    y1+ad+y2*11,0,BGN_COLOR,BGN_COLOR); // 5.晚上
  //选中背景
-  TRect rc={ww*2+4,y1+y2*(point_y+1)+1,ww*8+4,y1+y2*(point_y+2)};
-  if(daylist==0){
-    rc=SetRect(ww*(point_x+1)+4,y1+y2*(point_y+1)+1,ww*(point_x+2)+4,y1+y2*(point_y+2));  
+  if(dlt==0){
+    if(p_x==7) p_x=6;
+    rc=SetRect(ww*(p_x+1)+4,y1+y2*(p_y+1)+1,ww*(p_x+2)+4,y1+y2*(p_y+2));  
+  }else{
+    rc=SetRect(ww*2+4,y1+y2*(p_y+1)+1,ww*8+4,y1+y2*(p_y+2));
   }
   DrawRectangle(rc.l,rc.t,rc.r,rc.b,0,BGC_COLOR,BGC_COLOR); 
   short L=rc.l,R=rc.r;
   //课程表线及课程数据
-  for(i=0;i<=7;i++){
-   for(j=0;j<=ac;j++){
+  for(byte i=0;i<=7;i++){
+   for(byte j=0;j<=ac;j++){
    //画线框
    DrawLine(Left, y1+ad+y2*(j+1),ww*(7+1)+3,y1+ad+y2*(j+1), 0,FNL_COLOR); //下
    DrawLine(Left, y1+ad*2,       Left,      y1+y2*(ac+1)+ad,0,FNL_COLOR); //左
    DrawLine(Right,y1+ad*2,       Right,     y1+y2*(ac+1)+ad,0,FNL_COLOR); //右
    rc=SetRect(ww*(i+1)+aa+2,y1+y2+ad,ww*(i+1)+aa+2,y1+ad+y2*(ac+1)); 
-   if(!daylist && i && i!=7){
+   if(!dlt && i && i!=7){
      DrawLine(rc.l,rc.t,rc.r,rc.b,0,FNL_COLOR); //其它竖线，短
    }else if(i==1){
      DrawLine(rc.l,rc.t,rc.r,rc.b,0,FNL_COLOR); //第二根竖线，重复，粗线
@@ -118,17 +118,17 @@ static void onRedraw(MAIN_GUI *data){
    if(j==0 && i>0){  
      wsprintf(ws,PNT_ONE,"时间");
      rc=SetRect(3,OHigh+3,ww*2+5,OHigh+YSize);
-     DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNW_COLOR,tsColor);
-     if(daylist==0){
-       wsprintf(ws,PNT_ONE,WeekID[i-1]);
+     DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNW_COLOR,TRAN_CBK);
+     if(dlt==0 && i!=7){       
        rc.l=LW;
        rc.r=LW+R-L;
-     }else if(daylist==1 && i==point_x){
-       wsprintf(ws,PNT_ONE,WeekGB[i-1]);
+       wsprintf(ws,PNT_ONE,WeekID[i-1]);
+     }else if(dlt==1 && i==p_x){       
        rc.l=L;
        rc.r=R;
+       wsprintf(ws,PNT_ONE,WeekGB[i-1]);
      }
-     DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNW_COLOR,tsColor);   
+     DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNW_COLOR,TRAN_CBK);   
    }
    //课程数据
    if(j<9){
@@ -138,22 +138,22 @@ static void onRedraw(MAIN_GUI *data){
     wsprintf(ws,PNT_ONE,Res);
     week=OHigh+j*(YSize+2)+4+YSize;
     rc=SetRect(LW,week,LW+R-L,week+YSize); 
-    if(daylist==1 && i==point_x){      
-      DrawString(ws,L,rc.t,R,rc.b,8,2,FNC_COLOR,tsColor);    
+    if(dlt==1 && i==p_x){      
+      DrawString(ws,L,rc.t,R,rc.b,8,2,FNC_COLOR,TRAN_CBK);    
     }else if(i==0){  
       rc=SetRect(3, week,ww*2+5,week+YSize);
-      DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNM_COLOR,tsColor);       
-    }else if(daylist==0 && i>0){
+      DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNM_COLOR,TRAN_CBK);       
+    }else if(dlt==0 && i>0){
       strncpy(TEMP,Res,2);      
       TEMP[2]=0;
       wsprintf(ws,PNT_ONE,TEMP);      
       if(i==GetWeek(&d)+1){
-         if(j+2==point_y)
-           DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNC_COLOR,tsColor);   
+         if(j+2==p_y)
+           DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNC_COLOR,TRAN_CBK);   
          else
-           DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FND_COLOR,tsColor);
+           DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FND_COLOR,TRAN_CBK);
       }else{
-        DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNC_COLOR,tsColor);      
+        DrawString(ws,rc.l,rc.t,rc.r,rc.b,8,2,FNC_COLOR,TRAN_CBK);      
       }
     }
    }
@@ -161,12 +161,26 @@ static void onRedraw(MAIN_GUI *data){
 }
 FreeWS(ws);
 }
-static void onCreate(MAIN_GUI *data, void *(*malloc_adr)(int)){
+static void onCreate(MAIN_GUI *data, void *(*malloc_adr)(int)){  
+  Data=LoadFileBuf(cData);
+  GetDateTime(&d, &t);
+  p_x=GetWeek(&d)+1;
+  if(p_x==7){dlt=1;Kcb=7;}
+  if(t.hour<=7) p_y=1; //morning 1
+  elif(t.hour<=11) p_y=t.hour%7+2; //morning 1-4
+  elif(t.hour<=14) p_y=6; //afternoon 5
+  elif(t.hour<=17) p_y=t.hour%10+2; //afternoon 5-7
+  elif(t.hour<=19) p_y=8;
+  elif(t.hour<=20) p_y=9;
+  elif(t.hour<=21) p_y=ac;
+  //elif(t.hour>=22) { p_y=1; p_x=GetWeek(&d)+2; } //next morning 1
+  else { p_y=1; p_x=GetWeek(&d)+2; }//p_y=8; //night 8
   data->gui.state=1;
 }
 
 static void onClose(MAIN_GUI *data, void (*mfree_adr)(void *)){
   data->gui.state=0;
+  FreeFileBuf(Data);
 }
 
 static void onFocus(MAIN_GUI *data, void *(*malloc_adr)(int), void (*mfree_adr)(void *)){
@@ -179,14 +193,18 @@ static void onUnfocus(MAIN_GUI *data, void (*mfree_adr)(void *)){
 }
 
 static int OnKey(MAIN_GUI *data, GUI_MSG *msg){
+  char TEMP[64];
   if (msg->gbsmsg->msg==KEY_DOWN){   
     switch(msg->gbsmsg->submess){
-      case UP_BUTTON:    if(point_y>1)  point_y--; else point_y=ac; REDRAW(); break;
-      case DOWN_BUTTON:  if(point_y<ac) point_y++; else point_y=1;  REDRAW(); break;
-      case LEFT_BUTTON:  if(point_x>1)  point_x--; else point_x=6;  REDRAW(); break;
-      case RIGHT_BUTTON: if(point_x<6)  point_x++; else point_x=1;  REDRAW(); break;
-      case ENTER_BUTTON: if(daylist==0) daylist=1; else daylist=0;  REDRAW(); break;
-      case LEFT_SOFT: REDRAW(); break;
+      case UP_BUTTON:    if(p_y>1)    p_y--; else p_y=ac; REDRAW(); break;
+      case DOWN_BUTTON:  if(p_y<ac)   p_y++; else p_y=1;  REDRAW(); break;
+      case LEFT_BUTTON:  if(p_x>1)    p_x--; else p_x=Kcb;REDRAW(); break;
+      case RIGHT_BUTTON: if(p_x<Kcb)  p_x++; else p_x=1;  REDRAW(); break;
+      case ENTER_BUTTON: if(dlt==0){dlt=1;Kcb=7;}else{dlt=0;Kcb=6;}  REDRAW(); break;
+      case LEFT_SOFT:sprintf(TEMP,"%s\n%s",ver,"修改:玫瑰!\n感谢:\nzhangxxx");
+                     LockSched();
+                     ShowMSG(1,(int)TEMP);  
+                     UnlockSched();break;
       case GREEN_BUTTON:RunAPP((char *)cfn);break;
       case RED_BUTTON:
       case RIGHT_SOFT: CloseCSM(MAINCSM_ID); break;
@@ -213,15 +231,19 @@ const void * const gui_methods[11]={
 };
 
 static int maincsm_onmessage(CSM_RAM *data, GBS_MSG *msg){
+  char TEMP[64];
   MAIN_CSM *csm=(MAIN_CSM*)data;
   if(msg->msg == MSG_RECONFIGURE_REQ){    
     if(strcmp_nocase(cfn,(char *)msg->data0)==0){                
       InitConfig();
       //重新加载节日文件数据
-      if(FreeFileBuf(Data)) Data=LoadFileBuf(cData); 
-      ShowMSG(1,(int)ver);  
+      if(FreeFileBuf(Data)) Data=LoadFileBuf(cData);
+      sprintf(TEMP,"%s\n%s",ver,"参数已更新!");
+      LockSched();
+      ShowMSG(1,(int)TEMP);  
+      UnlockSched();  
     }
-  }   
+  }    
   if ((msg->msg==MSG_GUI_DESTROYED)&&((int)msg->data0==csm->gui_id))  {
     csm->csm.state=-3;
   }
@@ -244,14 +266,9 @@ static void maincsm_oncreate(CSM_RAM *data){
   csm->gui_id=CreateGUI(main_gui);  
 }
 
-static void Killer(void){
+static void maincsm_onclose(CSM_RAM *csm){
   extern void *ELF_BEGIN;
   kill_data(&ELF_BEGIN,(void (*)(void *))mfree_adr());
-}
-
-static void maincsm_onclose(CSM_RAM *csm){
-  FreeFileBuf(Data);
-  SUBPROC((void *)Killer);
 }
 
 const int minus11=-11;
@@ -286,27 +303,8 @@ static void UpdateCSMname(void){
   wsprintf((WSHDR *)(&MAINCSM.maincsm_name),PNT_ONE,ver);
 }
 
-int main(){  
+int main(void){  
   InitConfig();
-  Data=LoadFileBuf(cData);
-  //根据每日时间改变默认选中课程
-  GetDateTime(&d, &t);
-  point_x=GetWeek(&d)+1; 
-
-  if(t.hour<=7) point_y=1; //morning 1
-  else{
-    if(t.hour<=11) point_y=t.hour%7; //morning 1-4
-    else{
-      if(t.hour<=14) point_y=5; //afternoon 5
-      else{
-        if(t.hour<=17) point_y=t.hour%10; //afternoon 5-7
-        else{
-          if(t.hour>=22) { point_y=1; point_x=GetWeek(&d)+2; } //next morning 1
-          else point_y=8; //night 8
-      }
-     }
-    }
-  }
   LockSched();  
   char dummy[sizeof(MAIN_CSM)];
   UpdateCSMname();  
