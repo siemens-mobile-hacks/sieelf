e3a7dec0f0ddc89e7e034fb180a567a48760a4ce
loquat520@qq.com <loquat520@qq.com>
2010-05-11 02:51:34 +0000

    翻译了LGP

--- LaunchElf/elf/elf.h
+++ LaunchElf/elf/elf.h
@@ -3,25 +3,25 @@
 
 /*typedef struct
 {
-  char name[16];//имя шортката
-  void* pointer;//адрес функции
+  char name[16];//name shortcuts
+  void* pointer;//function address
   int unk_zero;
   int unk_1;
-  int lgp_full_name;//полное имя
+  int lgp_full_name;//full name
   int unk_2;
   int unk_3;
-  int lgp_short_name;//сокращенное имя
+  int lgp_short_name;//Short name
   int unk_4;
-  int lgp_comment_msg;//текст справки по этому шорткату, в патче - путь к эльфу
+  int lgp_comment_msg;//help text for this shortcut, in the patch - the path to elf
 }shortcut;*/
 
 const char percent_t[]="%t";
 const char percent_s[]="%s";
 const char txt_null[]="";
 
-char icon1[] = "0:\\Zbin\\img\\ELF_SMALL.png";//эльф
-char icon2[] = "0:\\Zbin\\img\\UNK_SMALL.png";//шорткат
-char icon3[] = "0:\\Zbin\\img\\ADD.png";//добавление нового эльфа
+char icon1[] = "0:\\Zbin\\img\\ELF_SMALL.png";//элelfьф
+char icon2[] = "0:\\Zbin\\img\\UNK_SMALL.png";//шоShortcut
+char icon3[] = "0:\\Zbin\\img\\ADD.png";      //adding a new elf
 
 const char shortcuts_filename[]="0:\\Zbin\\etc\\shortcuts.dat";
 const char shortcut_names_filename[]="0:\\Zbin\\etc\\shortcut_names.dat";
--- LaunchElf/elf/lgp.h
+++ LaunchElf/elf/lgp.h
@@ -1,18 +1,18 @@
 #ifndef LGP_H
 #define LGP_H
 
-const char LGP_ADDNEWELF[]="добавить эльф...";
-const char LGP_SAVE[]="Сохранить";
+const char LGP_ADDNEWELF[]="Add elf ...";
+const char LGP_SAVE[]="Save";
 const char LGP_ELFNAME[]="LaunchElf";
-const char LGP_EDITELFNAME[]="название эльфа:";
-const char LGP_ENITELFPATH[]="путь к эльфу:";
-const char LGP_EDITSHORTCUTNAME[]="название шортката:";
+const char LGP_EDITELFNAME[]="Elf name:";
+const char LGP_ENITELFPATH[]="path of the elves:";
+const char LGP_EDITSHORTCUTNAME[]="Shortcut name:";
 const char LGP_CLOSE[]="Close";
-const char LGP_SELECTFILE[]="Выбрать файл";
-const char LGP_NOTINSTALLED[]="патч не установлен!";
-const char LGP_NEWELFNAME[]="Эльф%d";
-const char LGP_ADDERROR[]="ошибка добавления!";
-const char LGP_ERRORLOADING[]="ошибка при загрузке настроек";
+const char LGP_SELECTFILE[]="Select File";
+const char LGP_NOTINSTALLED[]="Patch is not installed!";
+const char LGP_NEWELFNAME[]="Elf%d";
+const char LGP_ADDERROR[]="Error adding!";
+const char LGP_ERRORLOADING[]="Error loading configuration";
 //const char LGP_[]=;
 
 #endif
